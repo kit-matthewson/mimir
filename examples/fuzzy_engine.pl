32768ab4c361908d81b66c43c57fcdaed6efa3a1
Kit Matthewson <kit.matthewson@gmail.com>
2026-04-24 13:03:25 +0100

    Fuzzy engine example

---
+++ examples/fuzzy_engine.pl
@@ -0,0 +1,46 @@
+trap(X, A, _, _, _, F) :-
+  X < A,
+  F = 0.
+trap(X, A, B, _, _, F) :-
+  X >= A,
+  X <= B,
+  F = (X - A) / (B - A).
+trap(X, _, B, C, _, F) :-
+  X > B,
+  X < C,
+  F = 1.
+trap(X, _, _, C, D, F) :-
+  X >= C,
+  X <= D,
+  F = (D - X) / (D - C).
+trap(X, _, _, _, D, F) :-
+  X > D,
+  F = 0.
+
+not(X, Y) :-
+  Y = 1 - X.
+
+cold(T, F) :-
+  trap(T, -100, -100, 12, 15, F).
+hot(T, F) :-
+  trap(T, 22, 28, 100, 100, F).
+
+heating(on, T) :~
+  cold(T, F),
+  F.
+heating(off, T) :~
+  cold(T, F),
+  not(F, G),
+  G.
+
+cooling(on, T) :~
+  hot(T, F),
+  F.
+cooling(off, T) :~
+  hot(T, F),
+  not(F, G),
+  G.
+
+output(H, C, T) :-
+  heating(H, T),
+  cooling(C, T).
